4a0ba55ecdd562b41332ca4993ce2b2d912b2756
Cyberia-Place <eliasberto2@gmail.com>
2021-04-27 14:32:05 +0000

    domain generator finalizado

--- src/app.js
+++ src/app.js
@@ -6,6 +6,22 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let pronoun = ["the", "our"];
+  let adj = ["great", "big"];
+  let noun = ["jogger", "racoon"];
+  let dom = [".com", ".net", ".us", ".io"];
+
+  var string = "";
+  for (let pronWord of pronoun) {
+    for (let adjWord of adj) {
+      for (let nounWord of noun) {
+        for (let i = 0; i < 4; i++) {
+          console.log(i);
+          string = pronWord + adjWord + nounWord + dom[i];
+          console.log(string);
+          string = pronWord + adjWord + nounWord;
+        }
+      }
+    }
+  }
 };
